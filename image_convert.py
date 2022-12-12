7bc017c6ddfd959dd6239ee6f4e6a1f95a06c85a
Aurora Freir <37246948+CatAndDogSoup@users.noreply.github.com>
2022-12-12 17:57:55 +0000

    lil bit of cleanup on this code

--- image_convert.py
+++ image_convert.py
@@ -19,10 +19,6 @@ def get_pixels(image_name):
 
 
 # EYES
-for file_image_name in ["eye_static", "eye_backward", "eye_forward", "eye_blink", "eye_angry"]:
-    print(f"{file_image_name} =", get_pixels(f"{pathlib.Path(__file__).parents[0]}/{file_image_name}.png"))
-
-
-# MOUTH
-for file_image_name in ["mouth_closed", "mouth_open", "mouth_open_wide"]:
+for file_image_name in ["eye_static", "eye_backward", "eye_forward", "eye_blink", "eye_angry",
+                        "mouth_closed", "mouth_open", "mouth_open_wide"]:
     print(f"{file_image_name} =", get_pixels(f"{pathlib.Path(__file__).parents[0]}/{file_image_name}.png"))
